7c5e3b4d3b5c0fedd007e259eb2c0553b2debaa3
tracer-ics2021 <tracer@njuics.org>
2023-03-19 00:22:25 +0800

    >  compile 02114152    Static hostname: DESKTOP-7BT34I6          Icon name: computer-container            Chassis: container         Machine ID: 52f592799de848eee995506263ceee8a            Boot ID: 3a94182830d74139b8e549697cd7dcff     Virtualization: wsl   Operating System: Ubuntu 20.04.5 LTS             Kernel: Linux 5.15.90.1-microsoft-standard-WSL2       Architecture: x86-64  00:22:25 up 52 min,  1 user,  load average: 0.04, 0.01, 0.00

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -6,9 +6,11 @@
 #include <regex.h>
 
 enum {
-  TK_NOTYPE = 256, TK_EQ,
+  TK_NOTYPE = 256, TK_EQ,  //空格的类型
+  NUM,//十进制数字的类型
+  TK_ADD, REGNAME,NOTEQ,
 
-  /* TODO: Add more token types */
+  /* TODO: Add more token types */  //这里要添加更多的token类型
 
 };
 
@@ -17,13 +19,21 @@ static struct rule {
   int token_type;
 } rules[] = {
 
-  /* TODO: Add more rules.
+  /* TODO: Add more rules. 
    * Pay attention to the precedence level of different rules.
    */
 
   {" +", TK_NOTYPE},    // spaces
   {"\\+", '+'},         // plus
   {"==", TK_EQ},        // equal
+  {"[0-9]+",NUM},       //十进制数字
+  {"\\(",'('},          //左括号
+  {"\\)",')'},          //右括号
+  {"\\-",'-'},          
+  {"\\*",'*'},
+  {"\\/",'/'},
+
+  //还需添加十进制整数 - * / 和括号
 };
 
 #define NR_REGEX ARRLEN(rules)
@@ -33,7 +43,7 @@ static regex_t re[NR_REGEX] = {};
 /* Rules are used for many times.
  * Therefore we compile them only once before any usage.
  */
-void init_regex() {
+void init_regex() {  //初始化正则表达式
   int i;
   char error_msg[128];
   int ret;
@@ -52,10 +62,10 @@ typedef struct token {
   char str[32];
 } Token;
 
-static Token tokens[32] __attribute__((used)) = {};
-static int nr_token __attribute__((used))  = 0;
+static Token tokens[32] __attribute__((used)) = {};//用于按顺序存放已经被识别出的token信息
+static int nr_token __attribute__((used))  = 0;//nr_token指示已经被识别出的token数目.
 
-static bool make_token(char *e) {
+static bool make_token(char *e) { //用于识别token
   int position = 0;
   int i;
   regmatch_t pmatch;
@@ -79,8 +89,12 @@ static bool make_token(char *e) {
          * of tokens, some extra actions should be performed.
          */
 
-        switch (rules[i].token_type) {
-          default: TODO();
+        switch (rules[i].token_type) { //记录在成功识别出token后, 将token的信息依次记录到tokens数组中.
+          case TK_NOTYPE: //如果是空格 不用存
+          break;
+          default: strncpy(tokens[nr_token].str,substr_start,substr_len);
+                    nr_token+=substr_len;
+                    break;
         }
 
         break;
