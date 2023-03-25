5c22d3e2a58ca57fe507d29be067625892b3e163
tracer-ics2021 <tracer@njuics.org>
2023-03-25 19:42:58 +0800

    >  compile 02114152    Static hostname: DESKTOP-7BT34I6          Icon name: computer-container            Chassis: container         Machine ID: 52f592799de848eee995506263ceee8a            Boot ID: 53cd5b9d193048d896fa78501e14850c     Virtualization: wsl   Operating System: Ubuntu 20.04.5 LTS             Kernel: Linux 5.15.90.1-microsoft-standard-WSL2       Architecture: x86-64  19:42:58 up  2:24,  1 user,  load average: 0.00, 0.00, 0.00

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -112,6 +112,7 @@ static bool make_token(char *e) { //用于识别token
                   strncpy(tokens[nr_token].str,substr_start,substr_len);
                     tokens[nr_token].str[substr_len]='\0';
           default:  tokens[nr_token].type= rules[i].token_type;
+		  			++ nr_token;
                     break;
         }
 
